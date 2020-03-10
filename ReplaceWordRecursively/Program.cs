5dfa0fe239635c14b701fca06ae5e9b7f3cde075
Marius Iulian <marius.ancutescu@outlook.com>
2020-03-10 20:12:43 +0200

    Add project files.

---
+++ ReplaceWordRecursively/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ReplaceWordRecursively
+{
+    class Program
+    {
+        static void Main()
+        {
+            string text = Console.ReadLine();
+            string textToReplace = Console.ReadLine();
+            string textToReplaceWith = Console.ReadLine();
+            Console.WriteLine(RecursiveReplace(text, textToReplace, textToReplaceWith));
+        }
+
+        static string RecursiveReplace(string text, string textToReplace, string textToReplaceWith)
+        {
+            if (!text.Contains(textToReplace))
+            {
+                return text;
+            }
+
+            return text.Substring(0, text.IndexOf(textToReplace)) + textToReplaceWith + RecursiveReplace(text.Substring(text.IndexOf(textToReplace) + 1), textToReplace, textToReplaceWith);
+        }
+    }
+}
